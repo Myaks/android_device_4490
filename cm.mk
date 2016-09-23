25a6a0542dd00056904362a6eb4c1feefa058fd6
Максим <maksim-kurbanleev@yandex.ru>
2016-09-24 04:57:19 +0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,20 +2,20 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := mint2g
+PRODUCT_RELEASE_NAME := nano4
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/mint2g/full_mint2g.mk)
+$(call inherit-product, device/fly/nano4/full_nano4.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=mint \
-    TARGET_DEVICE=mint \
-    BUILD_FINGERPRINT="samsung/mintxx/mint:4.1.2/JZO54K/S5282XXANF3:user/release-keys" \
+    PRODUCT_NAME=nano \
+    TARGET_DEVICE=nano \
+    BUILD_FINGERPRINT="fly/mintxx/nano:4.1.2/JZO54K/S5282XXANF3:user/release-keys" \
     PRIVATE_BUILD_DESC="mintxx-user 4.1.2 JZO54K S5282XXANF3 release-keys"
     
 
-PRODUCT_NAME := cm_mint2g
-PRODUCT_DEVICE := mint2g
+PRODUCT_NAME := cm_nano4
+PRODUCT_DEVICE := nano4
